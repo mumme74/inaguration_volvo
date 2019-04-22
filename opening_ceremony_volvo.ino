0d9569c85dcbd2aba097a9df1cbfee43e10c25a1
Fredrik Johansson <fredrik@mumme.se>
2019-04-22 23:34:53 +0200

    Refactor to use FastLED_Action library

--- opening_ceremony_volvo.ino
+++ opening_ceremony_volvo.ino
@@ -30,112 +30,123 @@ CRGB leds_ch1[NUM_LEDS_CH1],
      leds_ch3[NUM_LEDS_CH3],
      leds_ch4[NUM_LEDS_CH4];
 
-Segment kuLogoLeft,
-        kuLogoMiddle,
-        kuLogoRight;
+CLEDController
+     *ch1 = &FastLED.addLeds<UCS1903, DATAOUT_CH1, BRG>(leds_ch1, NUM_LEDS_CH1),
+     *ch2 = &FastLED.addLeds<UCS1903, DATAOUT_CH2, BRG>(leds_ch2, NUM_LEDS_CH2),
+     *ch3 = &FastLED.addLeds<UCS1903, DATAOUT_CH3, BRG>(leds_ch3, NUM_LEDS_CH3),
+     *ch4 = &FastLED.addLeds<UCS1903, DATAOUT_CH4, BRG>(leds_ch4, NUM_LEDS_CH4);
 
-SegmentCompound kuLogo;
+
+SegmentPart kuLogoPart1(ch1, 10, 45),
+            kuLogoPart2(ch1, 60, 35),
+            kuLogoPart3(ch1, 105, 45),
+            connectStripPart1(ch1, 0, 10),
+            connectStripPart2(ch1, 56, 4),
+            connectStripPart3(ch1, 96, 9);
+
+Segment kuLogo,
+        liljasLogo,
+        volvoLogo,
+        connectStrip;
+
+SegmentCompound all;
 
 
-// https://forum.arduino.cc/index.php?topic=483504.0
 
-void clear();
-void setAllToColor(uint32_t col);
-void snake(uint32_t bgColor, uint32_t snakeColor, int fromLed, int toLed, int waitTime);
+// our main program goes in here
+void FastLED_Action::program() {
+
+  // setup kuLogoParts
+  kuLogoPart1.setLedController(ch1);
+  kuLogoPart2.setLedController(ch1);
+  kuLogoPart3.setLedController(ch1);
+
+  // kulogo
+  kuLogo.addSegmentPart(kuLogoPart1);
+  kuLogo.addSegmentPart(kuLogoPart2);
+  kuLogo.addSegmentPart(kuLogoPart3);
+
+  // connectStrip
+  connectStrip.addSegmentPart(connectStripPart1);
+  connectStrip.addSegmentPart(connectStripPart2);
+  connectStrip.addSegmentPart(connectStripPart3);
+
+  // actions
+  ActionSnake actSnake1(CRGB::Red, CRGB::White),
+              actSnake2(CRGB::Indigo, CRGB::Lavender),
+              actSnake3(CRGB::Lavender, CRGB::Indigo, true, true);
+  ActionColor actColorDarkGray(CRGB::DarkGray),
+              actColorDarkBlue(CRGB::DarkBlue),
+              actColorDarkGreen(CRGB::DarkGreen);
+  ActionFade actFade(5);
+  ActionColorLadder actLadder(CRGB::DarkGray, CRGB::Indigo);
+
+  // setup actions
+  kuLogo.addAction(actSnake1);
+  kuLogo.addAction(actColorDarkGray);
+  kuLogo.addAction(actColorDarkBlue);
+  kuLogo.addAction(actColorDarkGreen);
+  kuLogo.addAction(actSnake2);
+  kuLogo.addAction(actSnake3);
+  kuLogo.addAction(actFade);
+  kuLogo.addAction(actLadder);
+
+  // let the actions play out, repeat 5 times
+  for (uint8_t i = 0; i < 2; ++i)
+    kuLogo.yieldUntilAction(kuLogo.actionsSize());
+
+  // blow our load of glitter (steer airpressure vales)
+  FastLED_Action::clearAllActions();
+  ActionGotoColor actDark1(CRGB::Gray, CRGB::Black, 300),
+                  actWhite1(CRGB::Black, CRGB::WhiteSmoke, 200);
+  kuLogo.addAction(actDark1);
+  kuLogo.addAction(actWhite1);
+  kuLogo.yieldUntilAction(actWhite1);
+  digitalWrite(OUT_CH1, HIGH);
+  kuLogo.yieldUntilAction(actDark1);
+  digitalWrite(OUT_CH2, HIGH);
+  kuLogo.yieldUntilAction(2);
+  digitalWrite(OUT_CH1, LOW);
+  digitalWrite(OUT_CH2, LOW);
+
+}
 
 void setup() {
   delay(100); // allow to setup
-  CLEDController
-      *kuLeftC   = &FastLED.addLeds<UCS1903, DATAOUT_CH1, BRG>(leds_ch1, 10, 30),
-      *kuMiddleC = &FastLED.addLeds<UCS1903, DATAOUT_CH1, BRG>(leds_ch1, 42, 20),
-      *kuRightC  = &FastLED.addLeds<UCS1903, DATAOUT_CH1, BRG>(leds_ch1, 65, 40);
-  FastLED.addLeds<UCS1903, DATAOUT_CH2, BRG>(leds_ch2, NUM_LEDS_CH2);
-  FastLED.addLeds<UCS1903, DATAOUT_CH3, BRG>(leds_ch3, NUM_LEDS_CH3);
-  FastLED.addLeds<UCS1903, DATAOUT_CH4, BRG>(leds_ch4, NUM_LEDS_CH4);
+  Serial.begin(115200);
 
-  // setup controller
-  kuLogoLeft.addLedController(kuLeftC);
-  kuLogoMiddle.addLedController(kuMiddleC);
-  kuLogoRight.addLedController(kuRightC);
+  // FIXME need to solder 2 pulldown resistors to our inputs
+  // apparently mega2560 doesn't have pulldown
+  pinMode(INPUT_CH1, INPUT);
+  pinMode(INPUT_CH2, INPUT);
 
-  // kulogo
-  kuLogo.addSegment(&kuLogoLeft);
-  kuLogo.addSegment(&kuLogoMiddle);
-  kuLogo.addSegment(&kuLogoRight);
+  pinMode(OUT_CH1, OUTPUT);
+  pinMode(OUT_CH2, OUTPUT);
 }
 
+
+// main loop
 void loop() {
+  // sanity check, so we don't run our head straight through our stack
   static unsigned long secondsTick = 0;
-  if (secondsTick < millis()) {
+  int mem = freeMemory();
+  if (secondsTick < millis() || mem > 6144) {
     secondsTick = millis() + 1000;
     Serial.print("mem:"); Serial.println(freeMemory());
   }
 
+  static uint32_t programRunned = 0;
 
-   // Move a single white led
-   
-   for(int led = 0; led < NUM_LEDS_CH1; ++led) {
-      // Turn our current led on to white, then show the leds
-      leds_ch1[led] = CRGB::White;
-
-      // Show the leds (only one of which is set to white, from above)
-      FastLED.show();
-
-      // Wait a little bit
-      delay(100);
-
-      // Turn our current led back to black for the next loop around
-      leds_ch1[led] = CRGB::DarkRed;
-   }
-
-   for (int times = 0; times < 3; ++times) {
-      setAllToColor(CRGB::DarkGray);
-      delay(200);
-      setAllToColor(CRGB::DarkBlue);
-      delay(150);
-      setAllToColor(CRGB::DarkGreen);
-      delay(100);
-   }
-
-   snake(CRGB::Indigo, CRGB::Lavender, 0, NUM_LEDS_CH1, 40);
-
-   fadeToBlackBy(leds_ch1, NUM_LEDS_CH1, 20);
-
-   fill_gradient_RGB(leds_ch1, 2, CRGB::DarkGray, 30, CRGB::Indigo);
-
-   clear();
-   delay(500);
-}
-
-void snake(uint32_t bgColor, uint32_t snakeColor, int fromLed, int toLed, int waitTime) {
-  // set background
-  for (int led = fromLed; led < toLed; ++led) {
-    leds_ch1[led] = bgColor;
+  FastLED_Action::loop();
+  if (digitalRead(INPUT_CH1) && digitalRead(INPUT_CH2)) {
+    FastLED_Action::runProgram(false);
+    programRunned = millis();
   }
 
-  int previous = -1;
-  for (int led = fromLed; led < toLed + 1; ++led) {
-    if (led < toLed)
-      leds_ch1[led] = snakeColor;
-    if (previous > -1)
-      leds_ch1[previous] = bgColor;
-    previous = led;
-    FastLED.show();
-    delay(waitTime);
-  }
+  if (programRunned > 0 && programRunned + 120000 < millis())
+    programRunned = 0; // restart!
 }
 
-void setAllToColor(uint32_t col) {
-   for (int led = 0; led < NUM_LEDS_CH1; ++led)
-      leds_ch1[led] = col;
-   FastLED.show();
-  
-}
-
-void clear(){
-  // clear
-  setAllToColor(CRGB::Black);
-}
 
 
 
